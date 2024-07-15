c917d8dd36575eee24ba14f74085ac329642426d
mortalshot <seregagl1996@gmail.com>
2024-07-15 16:45:52 +0300

    buttons ripple effect

--- dist/js/app.js
+++ dist/js/app.js
@@ -8998,6 +8998,26 @@ PERFORMANCE OF THIS SOFTWARE.
                 targetElement.closest("body").classList.remove("_filter-active");
                 bodyUnlock();
             }
+            if (targetElement.closest("[data-ripple]")) {
+                const button = targetElement.closest("[data-ripple]");
+                const ripple = document.createElement("span");
+                const diameter = Math.max(button.clientHeight, button.clientWidth);
+                const radius = diameter / 2;
+                ripple.style.width = ripple.style.height = `${diameter}px`;
+                ripple.style.left = `${e.pageX - (button.getBoundingClientRect().left + scrollX) - radius}px`;
+                ripple.style.top = `${e.pageY - (button.getBoundingClientRect().top + scrollY) - radius}px`;
+                ripple.classList.add("ripple");
+                "once" === button.dataset.ripple && button.querySelector(".ripple") ? button.querySelector(".ripple").remove() : null;
+                button.appendChild(ripple);
+                const timeOut = getAnimationDuration(ripple);
+                setTimeout((() => {
+                    ripple ? ripple.remove() : null;
+                }), timeOut);
+                function getAnimationDuration() {
+                    const aDuration = window.getComputedStyle(ripple).animationDuration;
+                    return aDuration.includes("ms") ? aDuration.replace("ms", "") : 1e3 * aDuration.replace("s", "");
+                }
+            }
         }));
         const compareSwitch = document.querySelector(".compare__switch .switch__input");
         if (compareSwitch) compareSwitch.addEventListener("change", (function() {
--- src/js/files/script.js
+++ src/js/files/script.js
@@ -582,6 +582,33 @@ document.addEventListener('click', function (e) {
     targetElement.closest('body').classList.remove('_filter-active');
     bodyUnlock();
   }
+
+  if (targetElement.closest('[data-ripple]')) {
+    const button = targetElement.closest('[data-ripple]');
+    const ripple = document.createElement('span');
+    const diameter = Math.max(button.clientHeight, button.clientWidth);
+    const radius = diameter / 2;
+
+    ripple.style.width = ripple.style.height = `${diameter}px`;
+    ripple.style.left = `${e.pageX - (button.getBoundingClientRect().left + scrollX) - radius}px`;
+    ripple.style.top = `${e.pageY - (button.getBoundingClientRect().top + scrollY) - radius}px`;
+    ripple.classList.add('ripple');
+
+    button.dataset.ripple === 'once' && button.querySelector('.ripple') ? button.querySelector('.ripple').remove() : null;
+
+    button.appendChild(ripple);
+
+    const timeOut = getAnimationDuration(ripple);
+
+    setTimeout(() => {
+      ripple ? ripple.remove() : null;
+    }, timeOut);
+
+    function getAnimationDuration() {
+      const aDuration = window.getComputedStyle(ripple).animationDuration;
+      return aDuration.includes('ms') ? aDuration.replace("ms", '') : aDuration.replace("s", '') * 1000;
+    }
+  }
 })
 
 // Скрываем строки с одинаковыми значениями
